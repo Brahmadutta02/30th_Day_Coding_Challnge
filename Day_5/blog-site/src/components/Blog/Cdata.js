89df66b6b4e55a6d1c62a8c2e3a00ce1d7f50d71
brahmadutta02 <brahmaduttadalai012@gmail.com>
2025-02-15 22:25:10 +0530

    Day_5

---
+++ Day_5/blog-site/src/components/Blog/Cdata.js
@@ -0,0 +1,41 @@
+const Cdata = [
+    {
+      id: 1,
+      cover: "/Images/card1.jpg",
+      category: "Drinks Blog",
+      title: "Foods that are high in fat like red meat and umami flavors",
+    },
+    {
+      id: 2,
+      cover: "/Images/card2.jpg",
+      category: "Drinks Blog",
+      title: "Foods with big flavors like grilled or barbequed meats and sausages",
+    },
+    {
+      id: 3,
+      cover: "./Images/card3.jpg",
+      category: "Drinks Blog",
+      title: "A wide range of foods, from chicken and light meats to lightly spiced dark meats",
+    },
+    {
+      id: 4,
+      cover: "./Images/card4.jpg",
+      category: "Drinks Blog",
+      title: "Light seafood and grilled meaty fish (salmon, shark, swordfish)",
+    },
+    {
+      id: 5,
+      cover: "./Images/card5.jpg",
+      category: "Drinks Blog",
+      title: "Italian foods with herbs and tomato like pasta or pizza, rich roasted",
+    },
+    {
+      id: 6,
+      cover: "./Images/card6.jpg",
+      category: "Drinks Blog",
+      title: "Bold, flavorful foods, especially slow grilled or roasted meats",
+    },
+  ]
+  
+  export default Cdata
+  
---
+++ Day_5/blog-site/src/components/Slide/Sdata.js
@@ -0,0 +1,25 @@
+const Sdata = [
+  {
+    id: 1,
+    cover: "/Images/slide1.jpg",
+    category: "Drinks Blog",
+    title: "Pinot Noir",
+    desc: "A red wine that is light to medium-bodied and dry. It has flavors of strawberry, raspberry, and cherry.",
+  },
+  {
+    id: 2,
+    cover: "/Images/slide2.jpg",
+    category: "Drinks Blog",
+    title: "White wine",
+    desc: "ade from green or yellow grapes, white wine is pale or golden in color. ",
+  },
+  {
+    id: 3,
+    cover: "/Images/slide3.webp",
+    category: "Drinks Blog",
+    title: "Sparkling wine",
+    desc: "Made by fermenting wine in a bottle, which creates carbon dioxide bubbles.",
+  },
+]
+
+export default Sdata
